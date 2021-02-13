5bd15c81ec76e5341307be4827b2730598d3ee20
Maanvendra Singh <maansingh2618@gmail.com>
2021-02-13 17:22:11 +0530

    added yodish api and wire ended it with output div

--- app.js
+++ app.js
@@ -7,11 +7,30 @@ var textInput = document.querySelector("#txtInput");
 // initialising output div block
 var outputDiv = document.querySelector("#output");
 
+// initialising mock url
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
+// click event returns clickHandler callback after every click 
 btnTranslate.addEventListener("click", clickHandler)
 
+// callback function
 function clickHandler(){
-    console.log("clicked");
-    console.log("input: ",textInput.value);
-    outputDiv.innerText = "Maanvendra Singh"
+    // calling server for processing 
+    fetch(getTranslationURL(textInput.value))
+    .then(response=> response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
+// function to get Translation URL
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text;
+}
+
+// function for error handling
+function errorHandler(error){
+    console.log("error occured ",error);
+    alert("something's wrong with server. Please try in some time!");
 }
