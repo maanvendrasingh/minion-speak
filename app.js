86b923a303977fdb3400f25573a3286dc138af32
Maanvendra Singh <maansingh2618@gmail.com>
2021-02-13 13:09:55 +0530

    taking i/p & console
    taken i/p from text area and consoled it. Add CSS to the Web App.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert('app.js is working fine')
+// initialising translate button
+var btnTranslate = document.querySelector("#btn-translate");
+
+// initialising textInput
+var textInput = document.querySelector("#txtInput");
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input: ",textInput.value);
+}
