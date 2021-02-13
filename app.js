98ce081c1594b5ccb060780a4e5746dc8236f743
Maanvendra Singh <maansingh2618@gmail.com>
2021-02-13 16:50:44 +0530

    output div and script added

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var btnTranslate = document.querySelector("#btn-translate");
 // initialising textInput
 var textInput = document.querySelector("#txtInput");
 
+// initialising output div block
+var outputDiv = document.querySelector("#output");
+
 
 btnTranslate.addEventListener("click", clickHandler)
 
 function clickHandler(){
     console.log("clicked");
     console.log("input: ",textInput.value);
+    outputDiv.innerText = "Maanvendra Singh"
 }
